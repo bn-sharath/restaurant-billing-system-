3fe592b59c877b92a4331d2a59eb94d249ef5331
bn-sharath <sharathbn2001@gmail.com>
2021-08-13 18:38:44 +0530

    dynamically adding input tags using js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 
-addDish()
+function addDish()
 {
+    let dishes = document.getElementById("dishes")
+    let plates = document.getElementById("plates")
+    let price = document.getElementById("price")
+
+    let dishInput=document.createElement("input");
+    let plateInput=document.createElement("input");
+    let priceInput=document.createElement("input");
+    dishInput.type="text";
+    plateInput.type="text";
+    priceInput.type="text";
+
+    dishInput.className="items";
+    plateInput.className="items";
+    priceInput.className="items";
+    dishes.appendChild(dishInput);
+    plates.appendChild(plateInput);
+    price.appendChild(priceInput);
 
 }

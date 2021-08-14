0cc8482c2bc5c477e455fed68a3d3642a1c8b371
bn-sharath <sharathbn2001@gmail.com>
2021-08-14 17:39:49 +0530

    inserting thid to githup

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-
+let menu={
+    dosa:40,
+    idly:35,
+    meal:60
+};
 function addDish()
 {
     let dishes = document.getElementById("dishes")
@@ -19,4 +23,19 @@ function addDish()
     plates.appendChild(plateInput);
     price.appendChild(priceInput);
 
+}
+function total(){
+    // let price = document.getElementById("price");
+    let dishvalue = document.querySelectorAll("#dishes .items");
+    let platevalue = document.querySelectorAll("#plates .items");
+    let pricevalue = document.querySelectorAll("#price .items");
+    console.log(dishvalue);
+    console.log(platevalue);
+    console.log(pricevalue);
+    
+    for (const hotelDish in menu) {
+        console.log(hotelDish);
+        console.log(menu[hotelDish]);
+        
+    }
 }
